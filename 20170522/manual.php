0da0b54f9fe5cadf95dbcf42cfdd7d216bee5888
natsumeS <natsumeshun.kyoto@gmail.com>
2017-05-21 10:02:40 +0900

    add manual

---
+++ 20170522/manual.php
@@ -0,0 +1,61 @@
+<ul>
+<li>phpの場合分け例</li>
+
+	<?php
+
+	$x=5;
+	$string;
+	if($x>0){
+		$string='x is positive number';
+	}else if($x<0){
+		$string='x is negative number';
+	}else{
+		$string='x is zero';
+	}
+	?>
+	<p>出力-><?php echo $string; ?></p>
+
+<li>htmlのなかにphpを埋め込む</li>
+
+	<?php if($x==0):?>
+	<p>変数xが0のときのみ表示されます</p>
+	<?php elseif($x!=0):?>
+	<p>変数xが0でないときに表示されます</p>
+	<?php endif;?>
+
+<li>連想配列の利用例</li>
+
+<?php
+$y=array("name"=>"Taro",
+	"id"=>321,
+	"email"=>"taro@g.com",
+	"image"=>"../20170508/1.png");
+	?>
+	<p>username:<?php echo $y["name"];?></p>
+	<p>id:<?php echo $y["id"];?></p>
+	<img src='<?php echo $y["image"];?>'>
+
+<li>(応用)多次元連想配列</li>
+
+	<?php 
+	$data=array("user1"=>array("name"=>"Taro","image"=>"../20170508/1.png"),
+		"user2"=>array("name"=>"Jiro","image"=>"../20170508/2.png"));
+	?>
+	<p><?php echo $data["user2"]["name"];?></p>
+
+<li>foreach文</li>
+
+	<?php foreach($data as $user):?>
+	<p>name:<?php echo $user["name"];?></p>
+	<img src='<?php echo $user["image"];?>'>
+	<p>----------------------</p>
+	<?php endforeach;?>
+
+<p>キーも取得する</p>
+
+	<?php foreach($y as $key=>$value):?>
+	<p><?php echo $key;?>:<?php echo $value;?></p>
+	<?php endforeach;?>
+
+</ul>
+
